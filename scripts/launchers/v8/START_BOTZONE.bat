1f4d3935651ef449ebe340d9a7a9ce981152381a
Philsz-co <philsz-co@gitee.com>
2026-08-20 12:54:06 +0800

    docs(GUA-252): 登记敌<=5出单可拆TWT修复 + START_BOTZONE.bat 索引

---
+++ scripts/launchers/v8/START_BOTZONE.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d D:\GuanDanscore\YiFeiAI-GD
+set PYTHONIOENCODING=utf-8
+python scripts\launchers\v8\run_v8_vs_botzone.py --user-id 6a6411aa27e7bf01db0291c5 --api-key 2897963415 >> logs\v8_vs_botzone_20260820_single.log 2>&1
